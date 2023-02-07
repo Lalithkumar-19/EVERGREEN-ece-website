d3be96955089b759205417e26b2235ef6e751a4e
M.Lalikth kumar <121875934+Lalithkumar-19@users.noreply.github.com>
2023-02-07 08:46:33 +0530

    My class content website
    This is the website that i had build in my initial days .This website having my class PDF's subject to subject and lesson to lesson .
    I dedicated this to my class.

---
+++ web.js
@@ -0,0 +1,55 @@
+function online() {
+    alert("YAHOO UR IN ONLINE ");
+}
+
+function offline() {
+    alert("PLEASE CHECK UR INTERNET CONNECTION,THE loaders are will not load");
+}
+
+function demc() {
+    var data = " <button type='button' style=' color:blue; background-color:yellow; width:90px;height:40px'><a href='https://drive.google.com/file/d/1pGbEKMHQzZh99DABwMzWO7-fJnycQWMF/view?pli=1'>UNIT1</a></button> <button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/drive/folders/1jmddt7YRdgOafI7px70u9N8ngcNKrw6L'>UNIT2</a></button><br><button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/drive/folders/1jp-Ly9vEu5YQn6XepinOBV1AnEAXEOEN'>UNIT3</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/drive/folders/1jtIfY-aAf1BdVr56yR10w_mD2QIhgyr_'>UNIT4</a></button><br><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/drive/folders/1jtIfY-aAf1BdVr56yR10w_mD2QIhgyr_'>UNIT5</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/1BCLBN3XilIRQS3rWz4RuVDoaAc7SCCj5/view'>UNIT6</a></button>";
+
+    document.getElementById("lessons").innerHTML = data;
+}
+
+
+function Ep() {
+    var data = " <button type='button' style=' color:blue; background-color:yellow; width:90px;height:40px'><a href='https://drive.google.com/file/d/1MsxrGEA3May8lJ11m6bHI-aY3lQpJU_5/view'>UNIT1</a></button> <button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/file/d/17wd_NdLq_WTHNmSZpPPQM85XCbBGwBS5/view'>UNIT2</a></button><br><button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/file/d/1QnhQUHPVBN5gasCvV86XIY-N11g6LZtV/view'>UNIT3</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/19iig4Dg6UwXEoEfmlWBLjl53ZEDTQXiK/view'>UNIT4</a></button><br><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/drive/folders/1j6PQ8kTdgt88sB1-R8cZXiDxI-hLtU3Q'>UNIT5</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/drive/folders/1jRdNh4qrCqVaf0UlihR3fkDJFYWYYXQh?usp=sharing'>UNIT6</a></button>";
+
+    document.getElementById("lessons").innerHTML = data;
+
+}
+
+
+function pds() {
+    var data = " <button type='button' style=' color:blue; background-color:yellow; width:90px;height:40px'><a href='https://drive.google.com/drive/folders/1YbxEmVVa_Dv2wmH2Z6Mc2gaIkodrvdUE'>UNIT1</a></button> <button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/file/d/1YUuXTIXkfRt9FLrm79F7I-1HdrHHOC15/view'>UNIT2</a></button><br><button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/drive/folders/1b5A1HC4bQLCoFAb-zbpzviIkuFU03HVl?usp=sharing'>UNIT3</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/drive/folders/1b5BG-jFAAp-G0G6XChO3sbCWJ4Itd-qv?usp=sharing'>UNIT4</a></button><br><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/1seWMJDIfkkfEdYzrWDIst-dnLvcNL3eh/view'>UNIT5</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/1sgBxto0d5jP3G3Z-DpImbZ7xO4FSbHmw/view'>UNIT6</a></button>";
+
+    document.getElementById("lessons").innerHTML = data;
+
+}
+
+function Et() {
+    var data = " <button type='button' style=' color:blue; background-color:yellow; width:90px;height:40px'><a href='https://drive.google.com/file/d/1R24xi8cgNMC9k29VBUfqGVjH5Ik1WirM/view'>UNIT1</a></button> <button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/file/d/1Q-GEJ-Hp4abIFiuk45MDima41zHaQOcW/view'>UNIT2</a></button><br><button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/file/d/1Pki0JEwmUk6VtkG0BxZuPYDaCUPLoR44/view'>UNIT3</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/1PpkyMUPbgcOG0oM1E5ZtL3-EasF9ACBZ/view'>UNIT4</a></button><br><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/1PtK6XuRiCdDNU-b8_0CSbXTg6IKSH_gw/view'>UNIT5</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/1PvAEEk96lZxi0mvaRmWlw6R07fQpgCkO/view'>UNIT6</a></button>";
+
+    document.getElementById("lessons").innerHTML = data;
+
+}
+
+function ss() {
+    var data = " <button type='button' style=' color:blue; background-color:yellow; width:90px;height:40px'><a href='https://drive.google.com/file/d/1WyA3gCj44WL4na4MHvUAMtVRyO9BUmo3/view'>UNIT1</a></button> <button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/file/d/1WspgnqIQgYh6fNqO8sWvhWJwGxQjmfVs/view'>UNIT2</a></button><br><button type='button' style=' color:blue; background-color:yellow;width:90px;height:40px'><a href='https://drive.google.com/file/d/1QR3xQTZ7A95bPFE1Gk7hedoZok-0oJpW/view'>UNIT3</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/1QgwAsW9WLPA10XHB1ykq5hN4k_HZQ3Q6/view'>UNIT4</a></button><br><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/1QfEi78L49v8OiLa33E_eYKUjqYEKjDkw/view'>UNIT5</a></button><button style='color:blue; background-color:yellow;width:90px;height:40px' type='button'><a href='https://drive.google.com/file/d/1QWoe66b3gfjZtGKxFHSGO8c-jVOgp7CV/view'>UNIT6</a></button>";
+
+    document.getElementById("lessons").innerHTML = data;
+
+}
+
+function username() {
+    let name = prompt("PLEASE ENTER UR NAME PLEASE:", " ");
+    if (name) {
+        document.getElementById("welcome_bar").innerHTML = " <h3 style='color:white;background-color:black;'> HI  WELCOME " + "<span style='color:white;background-color:black;text-align:center;' >" + name + "</span>' </h3><br>";
+        document.getElementById("lessons").innerHTML = " <h3 style='color:white;background-color:black;'> HI  WELCOME " + "<span style='color:white;background-color:black;text-align:center;' >" + name + "</span>' </h3><br>";
+    }
+    if (!name) {
+        document.getElementById("lessons").innerHTML = " <h3 style='color:#0085db'>WELCOME  RGUKTAIN</h3>";
+        document.getElementById("welcome_bar").innerHTML = " <h3 style='color:#0085db'>WELCOME  RGUKTAIN</h3>";
+    }
+}
